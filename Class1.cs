3ca8210bb0a3ae65c80b2eebc5d6515528938adf
PPeachPie <56588811+PPeachPie@users.noreply.github.com>
2020-11-26 23:47:43 +1100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApplication2
+{
+    class Kletka
+    {
+        int size, sum;
+        bool[,] pop;
+
+        public Kletka(int sum, int size)
+        {
+            this.size = size;
+            this.sum = sum;
+            pop = new bool[size, size];
+            Colony();
+        }
+
+        public int SUM
+        {
+            get { return sum; }
+        }
+
+        public int SIZE
+        {
+            get { return size; }
+        }
+
+        public bool this[int x, int y]
+        {
+            get { return pop[x, y]; }
+            set { pop[x, y] = value; }
+        }
+        
+        public bool[,] Pop 
+        {
+            get { return pop;}
+            set { pop = value; }
+        }
+
+        void Colony()
+        {
+
+            int x, y, count = 0;
+
+            Random rnd = new Random();
+            if (sum>= size * size)
+            {
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                    {
+                        pop[i, j] = true;
+                    }
+            }
+            else
+            {
+                while (count < sum)
+                {
+                    x = rnd.Next(size);
+                    y = rnd.Next(size);
+                    if (pop[x, y] != true)
+                    {
+                        pop[x, y] = true;
+                        count++;
+                    }
+                };
+            }
+        }
+
+        public int Neigh(int x, int y)
+         {
+             int nb = 0;
+             if (this.pop[check(x - 1), check(y - 1)] == true) nb++;
+             if (this.pop[check(x-1), check(y)] == true) nb++;
+             if (this.pop[check(x - 1), check(y + 1)] == true) nb++;
+             if (this.pop[check(x), check(y-1)] == true) nb++;
+             if (this.pop[check(x), check(y + 1)] == true) nb++;
+             if (this.pop[check(x + 1), check(y-1)] == true) nb++;
+             if (this.pop[check(x + 1), check(y)] == true) nb++;
+             if (this.pop[check(x + 1), check(y+1)] == true) nb++;
+             return nb;
+         }
+
+        int check(int koord)
+        {
+            int k=koord;
+            if (koord < 0) k = size - 1;
+            if (koord > size - 1) k = 0;
+            return k;
+        }
+
+       public void Print( DataGridView dgv)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (this.pop[i, j] == true)
+                    {
+                        dgv.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Green;
+                    }
+                }
+            }
+        }
+
+       public void CleanDgv(DataGridView dgv)
+       {
+           for (int i = 0; i < dgv.RowCount; i++)
+           {
+               for (int j = 0; j < dgv.ColumnCount; j++)
+               {
+                   dgv.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.White;
+               }
+           }
+       }
+
+        public void ReColorDgv(DataGridView dgv, int x, int y)
+        {
+            dgv.Rows[x].Cells[y].Style.BackColor = System.Drawing.Color.White;
+        }
+
+        public int AliveCells()
+        {
+            int c = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (this.pop[i, j] == true)
+                    {
+                        c++;
+                    }
+                }
+            }
+            return c;
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+        }
+
+        Kletka colony1;
+        Kletka colony2;
+        int n, amount;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            input();
+            dgvsize();
+            colony1.Print(dgv1);
+           // life();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            btn1.Enabled = false;
+            tb1.Enabled = false;
+            tb2.Enabled = false;
+            button1.Enabled = true;
+            colony2.CleanDgv(dgv2);
+            life();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            btn1.Enabled = true;
+            button1.Enabled = false;
+            btn3.Enabled = false;
+            tb1.Enabled = true;
+            tb2.Enabled = true;
+            colony1.CleanDgv(dgv1);
+            colony2.CleanDgv(dgv2);
+            input();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            btn4.Enabled = true;
+            timer1 = new Timer(); // опять создаем новый таймер (не забудем про визуальный компонент
+            timer1.Interval = 1000; //ставим интервал события
+            timer1.Tick += btn2_Click;     //Создаем событие
+            timer1.Enabled = true;	//включаем таймер
+    }
+
+        public void life()
+        {
+            colony2.Pop = (bool[,]) colony1.Pop.Clone();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int neigh = colony1.Neigh(i,j);
+                    if (neigh == 2)
+                    {
+                        colony2[i, j] = colony1[i, j];
+                        continue;
+                    }
+                    else
+                    { 
+                        if (neigh > 3 || neigh <= 1)
+                    {
+                        colony2[i, j] = false;
+                        continue;
+                    }
+                    else
+                    {
+                        if (neigh == 3)
+                        {
+                            colony2[i, j] = true;
+                            continue;
+                        }
+                    }
+                    }
+                }
+            }
+            
+            colony2.Print(dgv2);
+            colony1 = colony2;
+          //  colony2 = new Kletka(0, colony1.SUM);
+            
+        }
+
+        public void dgvsize()
+        {
+            dgv1.RowCount = n;
+            dgv1.ColumnCount = n;
+            dgv2.RowCount = n;
+            dgv2.ColumnCount = n;
+            dgv1.Rows[0].Cells[0].Selected = false;
+            dgv2.Rows[0].Cells[0].Selected = false;
+            dgv1.Height = 400;
+            dgv1.Width = 400;
+            dgv2.Height = 400;
+            dgv2.Width = 400;
+            dgv1.RowHeadersVisible = false;
+            dgv1.ColumnHeadersVisible = false;
+            dgv2.RowHeadersVisible = false;
+            dgv2.ColumnHeadersVisible = false;
+
+            int razmer = 400 / n;
+            for (int i = 0; i < n; i++)
+            {
+                dgv1.Rows[i].Height = razmer;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                dgv1.Columns[i].Width = razmer;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                dgv2.Rows[i].Height = razmer;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                dgv2.Columns[i].Width = razmer;
+            }
+        }
+
+        public void input()
+        {
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            n = Convert.ToInt32(tb1.Text);//размерность поля
+            amount = Convert.ToInt32(tb2.Text);//количество клеток
+            colony1 = new Kletka(amount, n);
+            colony2 = new Kletka(0, n);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void dgv1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
